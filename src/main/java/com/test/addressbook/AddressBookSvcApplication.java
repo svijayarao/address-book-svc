7996e6c445c95bcc52dddb4445a3f1e3b7fbee6d
srinivas <srinivasvijayarao@giffgaff.co.uk>
2020-02-02 20:19:33 +0000

    first commit

---
+++ src/main/java/com/test/addressbook/AddressBookSvcApplication.java
@@ -0,0 +1,13 @@
+package com.test.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookSvcApplication.class, args);
+	}
+
+}
